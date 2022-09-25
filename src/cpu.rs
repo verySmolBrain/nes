741e85e9f73261c8e81058526705bce582b3af7c
smolbrain <brian.huang1@student.unsw.edu.au>
2022-09-25 12:42:17 +0000

    initial

---
+++ src/cpu.rs
@@ -0,0 +1,19 @@
+pub struct CPU {
+    pub register_a: u8, 
+    pub status: u8,
+    pub program_counter: u16,
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        CPU {
+            register_a: 0, 
+            status: 0, 
+            program_counter:0
+        }
+    }
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        todo!("Implement CPU interpreter");
+    }
+}
