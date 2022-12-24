cd7da0453c9da9e49da793fa6e664faee35024f2
smolbrain <randomlyrandamu@gmail.com>
2022-12-24 16:40:58 +1100

    Stack stuff

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,9 +1,12 @@
 use crate::opcodes::OPCODES;
 use bitflags::bitflags;
 
-const ADDRESS_SPACE: usize = 0xFFFF;
+const ADDRESS_SPACE: usize = 0xFFFF; // 64 KiB
 const ROM_START: usize = 0x8000;
 const RESET_VECTOR: usize = 0xFFFC;
+
+const STACK: u16 = 0x0100;
+const STACK_RESET: u8 = 0xff;
 
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
@@ -51,6 +54,7 @@ pub struct CPU {
     pub register_a: u8, 
     pub register_x: u8,
     pub register_y: u8,
+    pub stack_pointer: u8,
     pub status: Status,
     pub program_counter: u16,
     memory: [u8; ADDRESS_SPACE]
@@ -62,6 +66,7 @@ impl CPU {
             register_a: 0, // accumulator
             register_x: 0,
             register_y: 0,
+            stack_pointer: STACK_RESET,
             status: Status::empty(), 
             program_counter: 0,
             memory: [0; ADDRESS_SPACE]
@@ -143,6 +148,7 @@ impl CPU {
         self.register_a = 0;
         self.register_x = 0;
         self.register_y = 0;
+        self.stack_pointer = STACK_RESET;
         self.status = Status::empty();
 
         self.program_counter = self.mem_read_u16(RESET_VECTOR as u16);
