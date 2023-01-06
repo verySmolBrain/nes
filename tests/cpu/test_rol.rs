776fd406d8f0e1d7484837d1cd6cd8c759c192d7
smolbrain <randomlyrandamu@gmail.com>
2023-01-06 20:39:38 +1100

    Implemented ROL and tests

---
+++ tests/cpu/test_rol.rs
@@ -0,0 +1,58 @@
+#[cfg(test)]
+mod test {
+    use nes::cpu::CPU;
+    use nes::cpu::Status;
+
+    #[test]
+    fn test_0x2a_rol_accumulator_carry() {
+        let mut cpu = CPU::new();
+        cpu.load(vec![0x2a, 0x00]);
+        cpu.reset();
+
+        cpu.register_a = 0b1000_0000;
+        assert!(!cpu.status.contains(Status::CARRY));
+        
+        cpu.run();
+        assert!(cpu.status.contains(Status::CARRY));
+    }
+
+    #[test]
+    fn test_0x2a_rol_accumulator_set_from_carry() {
+        let mut cpu = CPU::new();
+        cpu.load(vec![0x2a, 0x00]);
+        cpu.reset();
+
+        cpu.register_a = 0b0000_0000;
+        cpu.status.insert(Status::CARRY);
+        
+        cpu.run();
+        assert_eq!(cpu.register_a, 0b0000_0001);
+    }
+
+    #[test]
+    fn test_0x2a_rol_accumulator_set_and_carry() {
+        let mut cpu = CPU::new();
+        cpu.load(vec![0x2a, 0x00]);
+        cpu.reset();
+
+        cpu.register_a = 0b1000_1111;
+        cpu.status.insert(Status::CARRY);
+        
+        cpu.run();
+        assert_eq!(cpu.register_a, 0b0001_1111);
+        assert!(cpu.status.contains(Status::CARRY));
+    }
+
+    #[test]
+    fn test_0x26_rol_zero_page_carry() {
+        let mut cpu = CPU::new();
+        cpu.load(vec![0x26, 0xa1, 0x00]);
+        cpu.reset();
+
+        cpu.memory[0xa1] = 0b1000_0000;
+        assert!(!cpu.status.contains(Status::CARRY));
+        
+        cpu.run();
+        assert!(cpu.status.contains(Status::CARRY));
+    }
+}
