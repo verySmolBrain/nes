add9bd21c297ac104cba28887536a9ce38ec63a4
smolbrain <randomlyrandamu@gmail.com>
2023-01-26 21:22:14 +1100

    Fixed clear tests

--- tests/cpu/test_clv.rs
+++ tests/cpu/test_clv.rs
@@ -1,20 +1,21 @@
 #[cfg(test)]
 mod test {
-    use nes::cpu::CPU;
+    use nes::cpu::Cpu;
     use nes::bus::Bus;
     use nes::cpu::Status;
+    use crate::helpers::{ TestRom, load_into_memory, check };
+    use expect_test::expect;
 
     #[test]
     fn test_b8_none() {
-        let bus = Bus::new();
-        let mut cpu = CPU::new(bus);
-        cpu.load(vec![0xb8, 0x00]);
-        cpu.reset();
+        let mut bus = Bus::new(TestRom::default_rom());
+        load_into_memory(&mut bus, vec![0xb8, 0x00], 0x0000);
 
+        let mut cpu = Cpu::new(bus);
+        cpu.program_counter = 0x0000;
         cpu.status.insert(Status::OVERFLOW);
         assert!(cpu.status.contains(Status::OVERFLOW));
         
-        cpu.run();
-        assert!(!cpu.status.contains(Status::OVERFLOW));
+        check(&mut cpu, expect![[""]])
     }
 }
