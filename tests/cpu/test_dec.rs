f7cc4ccdb89cb9e5580e412b9dc840c7de9d9aed
smolbrain <randomlyrandamu@gmail.com>
2023-01-27 17:33:20 +1100

    Fixed dex tests

--- tests/cpu/test_dec.rs
+++ tests/cpu/test_dec.rs
@@ -1,22 +1,20 @@
 #[cfg(test)]
 mod test {
-    use nes::cpu::CPU;
+    use nes::cpu::Cpu;
     use nes::bus::Bus;
     use nes::memory::Mem;
-    use crate::helpers::check_zero_and_negative;
+    use crate::helpers::{ TestRom, load_into_memory, check };
+    use expect_test::expect;
 
     #[test]
     fn test_c6_zero_page_zero() {
-        let bus = Bus::new();
-        let mut cpu = CPU::new(bus);
-        cpu.load(vec![0xc6, 0xa1, 0x00]);
-        cpu.reset();
+        let mut bus = Bus::new(TestRom::default_rom());
+        load_into_memory(&mut bus, vec![0xc6, 0xa1, 0x00], 0x0000);
+        bus.mem_write(0xa1, 0x01);
 
-        cpu.mem_write(0xa1, 0x01);
+        let mut cpu = Cpu::new(bus);
+        cpu.program_counter = 0x0000;
 
-        cpu.run();
-        
-        assert_eq!(cpu.mem_read(0xa1), 0x00);
-        check_zero_and_negative(cpu, 0)
+        check(&mut cpu, expect![[""]])
     }
 }
--- tests/cpu/test_dex.rs
+++ tests/cpu/test_dex.rs
@@ -1,21 +1,20 @@
 #[cfg(test)]
 mod test {
-    use nes::cpu::CPU;
+    use nes::cpu::Cpu;
     use nes::bus::Bus;
-    use crate::helpers::check_zero_and_negative;
+    use nes::cpu::Status;
+    use crate::helpers::{ TestRom, load_into_memory, check };
+    use expect_test::expect;
 
     #[test]
     fn test_ca_none_zero() {
-        let bus = Bus::new();
-        let mut cpu = CPU::new(bus);
-        cpu.load(vec![0xca, 0x00]);
-        cpu.reset();
+        let mut bus = Bus::new(TestRom::default_rom());
+        load_into_memory(&mut bus, vec![0xca, 0x00], 0x0000);
 
+        let mut cpu = Cpu::new(bus);
+        cpu.program_counter = 0x0000;
         cpu.register_x = 1;
 
-        cpu.run();
-        
-        assert_eq!(cpu.register_x, 0x00);
-        check_zero_and_negative(cpu, 0)
+        check(&mut cpu, expect![[""]])
     }
 }
