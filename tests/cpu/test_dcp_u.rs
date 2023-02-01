f35ca851a7a8a461d2b0031b68184f470598d262
smolbrain <randomlyrandamu@gmail.com>
2023-02-02 00:52:46 +1100

    Implemented dcp

--- tests/cpu/test_dcp_u.rs
+++ tests/cpu/test_dcp_u.rs
@@ -1,22 +1,28 @@
-// #[cfg(test)]
-// mod test {
-//     use nes::emulator::cpu::Cpu;
-//     use nes::emulator::bus::Bus;
-//     use crate::helpers::{ TestRom, load_into_memory, check };
-//     use expect_test::expect;
+#[cfg(test)]
+mod test {
+    use nes::emulator::cpu::Cpu;
+    use nes::emulator::bus::Bus;
+    use nes::emulator::memory::Mem;
+    use crate::helpers::{ TestRom, load_into_memory, check };
+    use expect_test::expect;
 
-//     #[test]
-//     fn test_cb_axs_u_immediate_write() {
-//         let mut bus = Bus::new(TestRom::default_rom());
-//         load_into_memory(&mut bus, vec![0xcb, 0x5, 0x00], 0x0000);
+    #[test]
+    fn test_c7_dcp_u_zero_page_subtract() {
+        let mut bus = Bus::new(TestRom::default_rom());
+        load_into_memory(&mut bus, vec![0xc7, 0xa1, 0x00], 0x0000);
+        bus.mem_write(0x00a1, 0x02);
 
-//         let mut cpu = Cpu::new(bus);
-//         cpu.program_counter = 0;
-//         cpu.register_a = 0b1111_1111;
-//         cpu.register_x = 0b0000_1010;
+        let mut cpu = Cpu::new(bus);
+        cpu.program_counter = 0;
 
-//         check(&mut cpu, expect![[r#"
-//             0000  CB 05     AXS_U #$05                      A:FF X:0A Y:00 P:24 SP:FD
-//             0002  00        BRK                             A:FF X:05 Y:00 P:24 SP:FD"#]]);
-//     }
-// }
+        check(&mut cpu, expect![[r#"
+            0000  C7 A1     DCP_U $A1 = 02                  A:00 X:00 Y:00 P:24 SP:FD
+            0002  00        BRK                             A:00 X:00 Y:00 P:24 SP:FD"#]]);
+
+        let val = cpu.bus.mem_read(0x00a1);
+        let expected = expect![[r#"
+            1
+        "#]];
+        expected.assert_debug_eq(&val);
+    }
+}
